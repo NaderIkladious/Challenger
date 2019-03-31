921aa2dac585ecc636ce45fc0a80e8400e5e6d4f
Nader Ikladious <nader.ikladious@gmail.com>
2019-03-31 19:57:25 +0200

    feat: add quizzes and questions tables

---
+++ database/migrations/2019_03_31_165406_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('question');
+            $table->longText('description');
+            $table->enum('type', ['text', 'mcq']);
+            $table->json('answer');
+            $table->float('score');
+            $table->unsignedInteger('quiz_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
