89c1454828ab5ebb67110ffcf52125fd15df5901
Nader Ikladious <nader.ikladious@gmail.com>
2019-03-31 22:26:25 +0200

    chore: add react preset

---
+++ resources/js/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return <div>Home page</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
