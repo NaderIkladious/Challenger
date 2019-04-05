6597b0d4a1c581667500a6f00b35d10e6c029118
Nader Ikladious <nader.ikladious@gmail.com>
2019-04-06 01:00:01 +0200

    feat: add api to get answer score

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -32,4 +32,30 @@ class QuestionController extends Controller
 
         return response()->json($question, 200);
     }
+
+    /**
+     * Check if the score of the answered question
+     *
+     * @param Request $request
+     * @return String the score of the answer
+     */
+    public function check(Request $request)
+    {
+        $validatedData = $request->validate([
+            'id'      => 'required',
+            'answer'   => 'required',
+        ]);
+
+        $question = Question::find($validatedData['id']);
+        $score = 0;
+        if ($question['type'] === 'mcq') {
+            $answer = json_decode($question['answer']);
+            $score = in_array($validatedData['answer'], $answer) ? $question['score'] : 0;
+        } else if ($question['type'] === 'text') {
+            $answer = json_decode($question['answer']);
+            $scorePerWord = (float)$question['score'] / count($answer);
+            $score = count(array_intersect($answer, explode(' ', $validatedData['answer']))) * $scorePerWord;
+        }
+        return response()->json($score, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,4 @@
 
 Route::get('quizzes/{id}', 'QuizController@show');
 Route::post('questions', 'QuestionController@store');
+Route::post('questions/validate', 'QuestionController@check');
